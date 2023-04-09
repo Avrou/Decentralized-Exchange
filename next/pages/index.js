11ab40d8ee57862c6904e86b995c2345ccbfedba
Avrou <avrouonline@gmail.com>
2023-04-09 10:54:16 -0700

    update

--- next/pages/index.js
+++ next/pages/index.js
@@ -482,15 +482,15 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Crypto Devs</title>
+        <title>Avrou Devs</title>
         <meta name="description" content="Whitelist-Dapp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.main}>
         <div>
-          <h1 className={styles.title}>Welcome to Crypto Devs Exchange!</h1>
+          <h1 className={styles.title}>Welcome to Avrou Devs Exchange!</h1>
           <div className={styles.description}>
-            Exchange Ethereum &#60;&#62; Crypto Dev Tokens
+            Exchange Ethereum &#60;&#62; Avrou Dev Tokens
           </div>
           <div>
             <button
@@ -518,7 +518,7 @@ export default function Home() {
       </div>
 
       <footer className={styles.footer}>
-        Made with &#10084; by Crypto Devs
+        Made with &#10084; by Avrou Devs
       </footer>
     </div>
   );
